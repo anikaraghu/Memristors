005b8ce7e4d35f651631d7e0f78c38a2078953c4
anikaraghu <anika.raghuvanshi@gmail.com>
2015-02-01 07:38:21 -0800

    01/31/15 - scripts for running MV algorithms

---
+++ run-MV2-subset.bat
@@ -0,0 +1,10 @@
+@echo off
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\3vars.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\5vars.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\carry.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\exam1_d.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\exam3_d.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\rd53f1.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\rd53f2.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\rd53f3.pla
+java -cp build\classes MemristorsMV2.MemristorsMV2 benchmarks\xor5_d.pla
